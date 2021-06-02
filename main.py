ebcd7b90646aca4a877cb9efa5080f9b8bb597d9
HotMonkeyWings <devsony52@gmail.com>
2021-06-02 07:51:00 +0530

    Plays sound now

--- main.py
+++ main.py
@@ -1,38 +1,61 @@
+from os import stat
 from selenium import webdriver
 from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+from datetime import datetime
 import time
+from playsound import playsound
 
-fireFoxOptions = Options()
-fireFoxOptions.headless = True
-browser = webdriver.Firefox(options=fireFoxOptions,executable_path='./geckodriver')
+def placeSelector(browser, state, district):
+    state = int(state)
+    district = int(district)
+    # Selects State
+    browser.find_element(By.ID,'mat-select-0').click()
+    xpath = '/html/body/div[2]/div[2]/div/div/div/mat-option[{}]/span'.format(state)
+    browser.find_element(By.XPATH,xpath).click()
 
-browser.get('https://www.cowin.gov.in/home')
-browser.find_elements(By.CLASS_NAME, 'mat-tab-label-content')[-1].click()
+    time.sleep(0.1)
 
-# Selects State
-browser.find_element(By.ID,'mat-select-0').click()
-browser.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div/div/mat-option[18]/span').click()
+    # Selects District
+    xpath = '/html/body/div[2]/div[2]/div/div/div/mat-option[{}]/span'.format(district)
+    browser.find_element(By.ID,'mat-select-2').click()
+    browser.find_element(By.XPATH, xpath).click()
 
-time.sleep(0.1)
+    # 1 - Alapuzha
+    # 2 - Ernakulam
+    # 5 - Kasargod
 
-# Selects District
-browser.find_element(By.ID,'mat-select-2').click()
-browser.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div/div/mat-option[2]/span').click()
+if __name__=='__main__':
 
-# 1 - Alapuzha
-# 2 - Ernakulam
-# 5 - Kasargod
+    fireFoxOptions = Options()
+    fireFoxOptions.headless = True
+    browser = webdriver.Firefox(options=fireFoxOptions,executable_path='./geckodriver')
+    browser.get('https://www.cowin.gov.in/home')
 
-browser.find_element(By.XPATH,'/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/form/mat-tab-group/div/mat-tab-body[3]/div/div/div[3]/button').click()
-browser.find_element(By.XPATH,'/html/body/app-root/div/app-home/div[2]/div/appointment-table/div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[2]/div[1]/div/div[1]/label').click()
+    while 1:
+        browser.find_elements(By.CLASS_NAME, 'mat-tab-label-content')[-1].click()
 
-available = browser.find_elements(By.CLASS_NAME,'dosetotal')
-if len(available) > 0:
-    print("Doses available")
-else:
-    print("No doses available")
+        placeSelector(browser, 18, 2)
 
-# browser.quit()
+        # Hit Search
+        browser.find_element(By.XPATH,'/html/body/app-root/div/app-home/div[2]/div/appointment-table/       div/div/div/div/div/div/div/div/div/div/form/mat-tab-group/div/mat-tab-body[3]/div/div/div[3]   /button').click()
+
+        # Select 18+
+        browser.find_element(By.XPATH,'/html/body/app-root/div/app-home/div[2]/div/appointment-table/       div/div/div/div/div/div/div/div/div/div/div[2]/form/div/div/div[2]/div[1]/div/div[1]/label').   click()
+
+        available = browser.find_elements(By.CLASS_NAME,'dosetotal')
+
+        currentTime = datetime.now()
+        currentTime = currentTime.strftime("%H:%M:%S")
+        if len(available) > 0:
+            print(str(currentTime) + ": Doses available")
+            playsound('./notif.mp3')
+        else:
+            print(str(currentTime) + ": No doses available")
+
+        time.sleep(30)
+        browser.refresh()
+    
+    browser.quit()
